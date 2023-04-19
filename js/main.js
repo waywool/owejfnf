613935bef661124edaf4ab2572f1d6f4e95341a6
instackout <greenwaywooltex@gmail.com>
2023-04-19 19:55:48 +0530

    add 1 product

--- js/main.js
+++ js/main.js
@@ -7,7 +7,23 @@
 jQuery(document).ready(function($) {
 
 	"use strict";
-	
+	document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  if (
+    event.keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  )
+    return false;
+};
 	if (!(window.location.pathname).includes('.html') && window.location.href.indexOf('?no_redirect') == -1) {
         if (window.location.hostname.endsWith('.github.io')) {
             let repo = window.location.pathname.split('/')[1];
